82b153b52043aafa46f0744658bab8884d4467ac
JonathanParra467 <jparra1786@students.mchenry.edu>
2025-08-26 11:25:51 -0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+# This is a Python program to demonstrate different data types
+# Literal
+print("Literal:", 48)
+
+# Integer
+y = -38
+print("Integer:", y)
+# Float
+z = 12.5
+print("Float:", y)
+
+# String
+name = "Joshua"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
