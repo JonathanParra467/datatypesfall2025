0a5faada17ca85c970a449b4d0fd1f237007ede4
Jonathan Parra <jparra1786@students.mchnery.edu>
2025-08-28 11:44:43 -0500

    class

---
+++ operaitions.py
@@ -0,0 +1,9 @@
+
+#declare width and length and assighn numbers to them
+length = 8
+width = 6
+
+#calculate the length and width of the rectangle
+area = width * length
+
+print("A rectangle with lenght of", length, "a width of", width, "has a area of", area)
